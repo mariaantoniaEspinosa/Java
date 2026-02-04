9018137fe4ad0d2a81f1e211df3c21646c639f16
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-02-04 16:44:59 -0300

    Create Operadores.java

---
+++ Aulas/Operadores.java
@@ -0,0 +1,36 @@
+public class OperadoresAritmeticos {
+    public static void main (String[] args){
+        int x = 10;
+        int y = 2;
+        
+        // x += y;
+        // x -= y;
+        // x *= y;
+        // x /= y;
+        
+        int z;
+        
+        //z = x + y;
+        //z = x - y;
+        // z = x * y;
+        // z = x / y;
+        // z = x % y;
+        
+        int i = 1;
+        
+        // i = i + 1;
+        // i += 1;
+        // i++;
+        
+        int j = 1;
+        
+        // i = i - 1;
+        // i -= 1;
+        // i--;
+        
+        System.out.println(z);
+        System.out.println(x);
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
