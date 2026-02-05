a2f43b62481630abc7c6146c398b7fba472653fd
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-02-05 20:53:30 -0300

    Update ifelse.java

--- Aulas/ifelse.java
+++ Aulas/ifelse.java
@@ -1,11 +1,33 @@
+import java.util.Scanner;
+
 public class IfElse {
     public static void main (String[] args){
         Scanner scanner = new Scanner(System.in);
 
         int age;
+        String name;
+        boolean isestudante;
+
+        System.out.print("Digite seu nome: ");
+        name = scanner.nextLine();
 
         System.out.print("Digite sua idade:" );
         age = scanner.nextInt();
+
+        System.out.print("Voce eh estudante? diga true or false ");
+        isestudante = scanner.nextBoolean();
+
+
+        //
+
+        if(name.isEmpty()){
+            System.out.println("voce nao respondeu seu nome");
+        } else{
+            System.out.println("\n heloo "+ name);
+        }
+
+
+        //
 
        if(age>=18){
             System.out.println("Voce eh adulto");
@@ -16,5 +38,12 @@ public class IfElse {
        } else {
             System.out.println("voce nao nasceu");
        }
+       //
+
+        if(isestudante == true){ // if(isestudante)
+            System.out.println("Voce eh estudante");
+        } else {
+            System.out.println("Voce nao eh estudante");
+        }
     }
 }
