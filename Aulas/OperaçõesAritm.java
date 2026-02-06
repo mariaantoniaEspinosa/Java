7d84299136d2cd47e97475a4cd6606860c16befb
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-02-05 21:29:19 -0300

    Create OperaçõesAritm.java

---
+++ Aulas/OperaçõesAritm.java
@@ -0,0 +1,28 @@
+public class Painel {
+    public static void main(String[] args){
+
+        //System.out.println(Math.PI);
+        //System.out.println(Math.E);
+
+        double resultado1,resultado2, resultado3, resultado4;
+        double resultado5, resultado6, resultado7, resultado8;
+
+        resultado1 = Math.pow(2, 3); //elevado a
+        resultado2 = Math.abs(-5); // valor absoluto - positivo
+        resultado3 = Math.sqrt(9); // raiz quadrada
+        resultado4 = Math.round(3.1); // transforma em inteiro
+        resultado5 = Math.ceil(3.14); // arredonda pra cima
+        resultado6 = Math.floor(3.99); // arredonda pra baixo
+        resultado7 = Math.max (10, 20); // qual eh o maior
+        resultado8 = Math.min(10, 20); //qual eh o menor
+
+        System.out.println(resultado1);
+        System.out.println(resultado2);
+        System.out.println(resultado3);
+        System.out.println(resultado4);
+        System.out.println(resultado5);
+        System.out.println(resultado6);
+        System.out.println(resultado7);
+        System.out.println(resultado8);
+    }
+}
