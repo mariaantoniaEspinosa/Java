fa2ae26633a6e483071ee56bd0cd29dae7b52c15
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-02-06 15:03:26 -0300

    Create 01ClasseSimples.java

---
+++ ExercíciosGPTpt2/01ClasseSimples.java
@@ -0,0 +1,18 @@
+public class n1 {
+    //criar uma classe com atributos e um metodo que exibe dados
+
+    //declaração da classe pessoa
+    static class Pessoa{
+
+        //atributo
+        String nome;
+        int idade;
+
+        //metodo
+        void exibirdados(){
+
+            System.out.println("nome: " + nome);
+            System.out.println("idade: " + idade);
+        }
+    }
+}
