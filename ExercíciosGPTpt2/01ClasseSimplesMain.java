17b2cc89b7d45528f0765ff546b3fb2b2fd38763
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-02-06 15:03:53 -0300

    Create 01ClasseSimplesMain.java

---
+++ ExercíciosGPTpt2/01ClasseSimplesMain.java
@@ -0,0 +1,10 @@
+public class n1main {
+    public static void main(String[] args){
+        n1.Pessoa p = new n1.Pessoa();
+
+        p.nome = "Maria";
+        p.idade = 20;
+
+        p.exibirdados();
+    }
+}
