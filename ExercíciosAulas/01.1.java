9f1c93aed8f7ecbb63bdade159072a4ed3a66808
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-02-21 21:02:01 -0300

    Create 01.1.java

---
+++ ExercíciosAulas/01.1.java
@@ -0,0 +1,9 @@
+public class laços1 {
+    //imprimir de 1 até 10
+    public static void main(String[]args){
+        int i;
+        for(i=0; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
