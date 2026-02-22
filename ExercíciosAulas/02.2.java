0adcb84e0f85641b68ea38daac8ed818d60cc409
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-02-21 21:07:36 -0300

    Create laços02 class to print even numbers
    Add a Java program to print even numbers from 2 to 20.

---
+++ ExercíciosAulas/02.2.java
@@ -0,0 +1,11 @@
+public class laços02 {
+    //imprimir pares de 2 ate 20
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 20; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
